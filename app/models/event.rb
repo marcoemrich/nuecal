7bdb92369f763a58d7e9cce91e9df3ec910554bf
Marco Emrich <marcoemrich77@googlemail.com>
2012-10-27 18:17:14 +0200

    event has json property allDay instead of allday, FIXED

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,11 @@
 class Event < ActiveRecord::Base
   attr_accessible :allday, :description, :end, :start, :summary, :title, :url, :location, :organizer
 
-  def to_json
+  def as_json(options = {})
     attr = attributes
-    attr[:allDay] = attributes[:allday]
-    attr.delete(:allday)
-    attr.to_json
+    attr["allDay"] = attributes["allday"]
+    attr.delete("allday")
+    attr
   end
+
 end
--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -1,8 +1,21 @@
 require_relative "../spec_helper"
 
 describe 'Event' do
-  it 'should return the allDay-Key on #to_json' do
-    e = Event.new
-    e.to_json.should match /"allDay"/
+  before :each do
+    @event= Event.new
   end
+
+  it 'should return the allDay-Key on #to_json' do
+    @event.to_json.should match /"allDay"/
+  end
+
+  it 'should not include allday (small letters)' do
+    @event.to_json.should_not match /"allday"/
+  end
+
+  it 'should have a default of false on allDay' do
+    @event.to_json.should match /"allDay":\s?false/
+  end
+
+
 end
