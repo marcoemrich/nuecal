919710b35d7ee8f3cb32c687d70eae6f2bc3e262
Marco Emrich <marcoemrich77@googlemail.com>
2012-10-27 13:10:05 +0200

    Rails skeleton created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuecal::Application.config.secret_token = 'acc08611db1d92499cebd80bf7f8a4e4f35ac67c329ddb0ad636b351a92254b6bf09d53a945b7cfe9bd0b1f481931ea13af7c997db0691b0b9b5846944505c78'
